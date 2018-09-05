7581ca1ba87fc5f79b3f16bf8861a41a8328b1e2
Ihor Babii <babiiihor91@gmail.com>
2018-09-05 10:18:54 +0300

    fix db generate

--- ToDoApp/Services/DbInitializerService.cs
+++ ToDoApp/Services/DbInitializerService.cs
@@ -12,14 +12,14 @@ namespace ToDoApp.Services
     {
         protected override void Seed(ToDoContext db)
         {
-            db.Profiles.Add(new Profile { FirstName = "Ihor", LastName = "Babii", ProfileId = new Guid(), Roles = new[] { Roles.Admin, Roles.User } });
+            db.Profiles.Add(new Profile { FirstName = "Ihor", LastName = "Babii", ProfileId = Guid.NewGuid(), Roles = new[] { Roles.Admin, Roles.User } });
             for (int i = 0; i < 20; i++)
             {
-                db.Tasks.Add(new Task { Deskription = $"Some deskription {i}", Name = $"Task {i}", Profile = db.Profiles.FirstOrDefault(), State = State.InProgres, TaskId = new Guid() });
+                db.Tasks.Add(new Task { Deskription = $"Some deskription {i}", Name = $"Task {i}", Profile = db.Profiles.FirstOrDefault(), State = State.InProgres, TaskId = Guid.NewGuid() });
             }
             for (int i = 0; i < 20; i++)
             {
-                db.Tasks.Add(new Task { Deskription = $"Some deskription {i+35}", Name = $"Task {i+35}", Profile = db.Profiles.FirstOrDefault(), State = State.Completed, TaskId = new Guid() });
+                db.Tasks.Add(new Task { Deskription = $"Some deskription {i + 35}", Name = $"Task {i + 35}", Profile = db.Profiles.FirstOrDefault(), State = State.Completed, TaskId = Guid.NewGuid() });
             }
             base.Seed(db);
         }
